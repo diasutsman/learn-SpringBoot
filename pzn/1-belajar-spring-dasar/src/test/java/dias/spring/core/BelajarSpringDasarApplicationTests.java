abd5c35577397d5c2f150e37584fc17b7358c238
Dias Utsman <utsmand91@gmail.com>
2024-07-14 21:12:29 +0700

    init commit

---
+++ pzn/1-belajar-spring-dasar/src/test/java/dias/spring/core/BelajarSpringDasarApplicationTests.java
@@ -0,0 +1,13 @@
+package dias.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringDasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
